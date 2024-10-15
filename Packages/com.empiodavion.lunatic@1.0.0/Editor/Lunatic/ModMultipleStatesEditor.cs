07f7d27d9353baa44edec33c8d0c08626eb4e9d1
Nicholas Stevens <empiodavion@gmail.com>
2024-10-15 11:24:41 +1100

    Added localisation display for NPC editor

--- Packages/com.empiodavion.lunatic@1.0.0/Editor/Lunatic/ModMultipleStatesEditor.cs
+++ Packages/com.empiodavion.lunatic@1.0.0/Editor/Lunatic/ModMultipleStatesEditor.cs
@@ -18,6 +18,9 @@ public class ModMultipleStatesEditor : ModBaseEditor
 
 	public override void DrawGUI()
 	{
+		EditorTools.DrawReloadLocalisation();
+		EditorTools.DrawTranslation($"Dialog/{id.stringValue} NAME", false);
+
 		EditorTools.DrawHelpProperty(id, "The id to use for saving and loading the state.");
 		EditorTools.DrawHelpProperty(value, "Sets which GameObject in STATES is active. Lunacid objects read this value from a zone string. Mod objects set it directly.");
 		EditorTools.DrawHelpProperty(states, "Array of inactive GameObjects that will have one set to active based on the value variable.");
